217701dcf2b3587d13bb217eeba6d63ba182d9d5
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-10 12:14:00 -0600

    Added ingredients

--- src/app/burrito-maker/burrito-maker.component.ts
+++ src/app/burrito-maker/burrito-maker.component.ts
@@ -10,7 +10,19 @@ export class BurritoMakerComponent implements OnInit {
 
   ingredients:BurritoIngredient[] = [
     new BurritoIngredient("flour-tortilla", 1),
-    new BurritoIngredient("corn-tortilla", 2)
+    new BurritoIngredient("corn-tortilla", 1),
+    new BurritoIngredient("white-rice", 2),
+    new BurritoIngredient("brown-rice", 2),
+    new BurritoIngredient("black-beans", 3),
+    new BurritoIngredient("pinto-beans", 3),
+    new BurritoIngredient("barbacoa", 4),
+    new BurritoIngredient("carnitas", 4),
+    new BurritoIngredient("chicken", 4),
+    new BurritoIngredient("steak", 4),
+    new BurritoIngredient("sofritas", 4),
+    new BurritoIngredient("cheese", 5),
+    new BurritoIngredient("fajita-vegetables", 5),
+    new BurritoIngredient("lettuce", 5),
   ]
 
   constructor() { }
