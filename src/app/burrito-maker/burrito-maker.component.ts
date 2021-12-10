fd7f68dd160108654a81eb18c5569e444e9407bd
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-10 16:43:35 -0600

    Added form

--- src/app/burrito-maker/burrito-maker.component.ts
+++ src/app/burrito-maker/burrito-maker.component.ts
@@ -29,6 +29,10 @@ export class BurritoMakerComponent implements OnInit {
   selectedIngredients:BurritoIngredient[] = []
 
   state:FormState = FormState.START
+  submitted: boolean = false;
+  firstNameError: boolean = false;
+  lastNameError: boolean = false;
+  emailError: boolean = false;
 
   constructor() { }
 
@@ -51,25 +55,42 @@ export class BurritoMakerComponent implements OnInit {
 
   add(ingredient:BurritoIngredient):void {
     if (this.selectedIngredients.some(selectedIngredient => ingredient.name === selectedIngredient.name)) {
-      this.selectedIngredients = this.selectedIngredients.filter(selectedIngredient => selectedIngredient.name !== ingredient.name);
+      if (ingredient.step === FormState.STEP_1) {
+        this.selectedIngredients = [];
+      } else {
+        this.selectedIngredients = this.selectedIngredients.filter(selectedIngredient => selectedIngredient.name !== ingredient.name);
+      }
     } else {
       if (ingredient.step === FormState.STEP_1 || ingredient.step === FormState.STEP_2 || ingredient.step === FormState.STEP_3) {
         const step = ingredient.step;
         if (!this.selectedIngredients.some(ingredient => ingredient.step === step)) {
           this.selectedIngredients.push(ingredient);
         } else {
-          this.selectedIngredients.splice(this.selectedIngredients.length - 1, 1, ingredient);
+          this.selectedIngredients.splice(
+            this.selectedIngredients.indexOf(
+              (this.selectedIngredients.find(selectedIngredient => selectedIngredient.step === step
+                ) as BurritoIngredient)), 1, ingredient);
         }
       } else if (ingredient.step === FormState.STEP_4) {
         if (this.selectedIngredients.filter(selectedIngredient => selectedIngredient.step === FormState.STEP_4).length < 2) {
           this.selectedIngredients.push(ingredient);
         } else {
-          this.selectedIngredients.splice(this.selectedIngredients.length - 2, 1);
+          this.selectedIngredients.splice(
+            this.selectedIngredients.indexOf(
+              (this.selectedIngredients.find(selectedIngredient => selectedIngredient.step === FormState.STEP_4
+                ) as BurritoIngredient)), 1);
           this.selectedIngredients.push(ingredient);
         }
       } else {
         this.selectedIngredients.push(ingredient);
       }
+    }
+    console.log(this.selectedIngredients);
+  }
+
+  onSubmit(isValid: boolean) {
+    if (isValid) {
+      this.submitted = true;
     }
   }
 
